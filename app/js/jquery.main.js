74aef95d2d98812a5401f7c8f1ca137821c59b91
alexserga <snike1985@gmail.com>
2017-04-06 17:14:51 +0300

    doing

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -3,8 +3,16 @@
 
     $(function(){
 
+        $('.contact-us').each( function() {
+            new ContactUs( $(this) );
+        } );
+
         $('.menu').each( function() {
             new Menu( $(this) );
+        } );
+
+        $('.site').each( function() {
+            new Site( $(this) );
         } );
 
         $('.reviews').each( function() {
@@ -21,11 +29,97 @@
 
     });
 
+
+    var ContactUs = function(obj) {
+
+        //private properties
+        var _obj = obj,
+            _background = _obj.find( '.contact-us__bg' ),
+            _window = $( window );
+
+        //private methods
+        var _addEvents = function() {
+
+                _window.on({
+                    'mousemove': function(e) {
+                        var pageX = e.pageX,
+                            pageY = e.pageY,
+                            halfWidth = _obj.width() / 2,
+                            halfHeight = _obj.height() / 2,
+                            percentFromCenterX = ( pageX - halfWidth ) / halfWidth,
+                            percentFromCenterY = ( pageY - halfHeight ) / halfHeight;
+
+                        _move( percentFromCenterX, 0 );
+                    }
+                });
+
+            },
+            _move = function( xPercent, yPercent ){
+                var backgroundStep = 10;
+
+                _background.css( {
+                    '-webkit-transform': 'translate( ' + -( xPercent * backgroundStep ) + 'px, ' + ( yPercent * backgroundStep ) + 'px )',
+                    'transform': 'translate( ' + -( xPercent * backgroundStep ) + 'px, ' + ( yPercent * backgroundStep ) + 'px )'
+                } );
+            },
+            _init = function() {
+                _addEvents();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
+    var Site = function(obj) {
+
+        //private properties
+        var _obj = obj,
+            _header = _obj.find( '.site__header' ),
+            _window = $( window );
+
+        //private methods
+        var _addEvents = function() {
+
+                _window.on({
+                    'load': function() {
+                        _changeHeaderType();
+                    },
+                    'scroll': function() {
+                        _changeHeaderType();
+                    }
+                });
+
+            },
+            _changeHeaderType = function() {
+                var winScroll = $( window ).scrollTop(),
+                    winHeight = $( window ).height();
+
+                if ( winScroll >= ( winHeight - _header.outerHeight() ) ) {
+                    _header.addClass( 'fixed' );
+                } else {
+                    _header.removeClass( 'fixed' );
+                }
+            },
+            _init = function() {
+                _addEvents();
+            };
+
+        //public properties
+
+        //public methods
+
+        _init();
+    };
+
     var Menu = function(obj) {
 
         //private properties
         var _obj = obj,
-            _btn = _obj.find( '.menu__btn' );
+            _btn = _obj.find( '.menu__btn' ),
+            _item = _obj.find( '.menu__item' );
 
         //private methods
         var _addEvents = function() {
@@ -38,6 +132,24 @@
                         } else {
                             _obj.removeClass( 'active' );
                         }
+                    }
+                });
+
+                _item.on({
+                    'click': function() {
+                        event.preventDefault();
+                        var elem = $( this ),
+                            id = elem.attr( 'href' ),
+                            way = $( id ).offset().top,
+                            duration = 1000;
+
+                        if ( !elem.hasClass( 'active' ) ) {
+                            $( 'body, html' ).animate( { scrollTop: way }, duration );
+                            _item.removeClass( 'active' );
+                            elem.addClass( 'active' );
+                            _obj.removeClass( 'active' );
+                        }
+
                     }
                 });
 
@@ -71,7 +183,6 @@
 
             },
             _sliderInit = function () {
-            console.log(1)
                 _sw = new Swiper( _slider, {
                     pagination: '.swiper-pagination',
                     paginationClickable: true
@@ -111,7 +222,7 @@
                     var curItem = $(this),
                         topPos = _obj.offset().top;
 
-                    if( _window.scrollTop() > (topPos - windowH/1.8) && !curItem.hasClass( 'animation' ) ){
+                    if( ( _window.scrollTop() + windowH*0.8 ) > topPos && !curItem.hasClass( 'animation' ) ){
 
                         curItem.addClass( 'animation' );
                     }
@@ -136,7 +247,9 @@
         var _obj = obj,
             _phone = _obj.find( '.hero__phone' ),
             _background = _obj.find( '.hero__bg' ),
-            _window = $( window );
+            _window = $( window ),
+            _winTop = _window.scrollTop(),
+            _siteHeader = $( '.site__header' );
 
         //private methods
         var _addEvents = function() {
@@ -165,6 +278,37 @@
                     }
                 });
 
+                _window.on({
+                    'scroll': function() {
+
+                        if ( _siteHeader.hasClass( 'fixed' ) ) {
+                            var direct = _window.scrollTop() > _winTop ? 'bottom' : 'top',
+                                maxScrollTop = $( '.site' ).height() - _window.height();
+
+                            if ( _window.scrollTop() > 0 ) {
+                                _winTop = _window.scrollTop();
+                            } else if ( _window.scrollTop() >= maxScrollTop ) {
+                                _winTop = maxScrollTop;
+                            } else {
+                                _winTop = 0;
+                            }
+
+                            // _winTop = _window.scrollTop() > 0 ? _window.scrollTop() : _window.scrollTop() >= maxScrollTop ? maxScrollTop : 0;
+
+                            switch ( direct ) {
+                                case 'top':
+                                    $( '.site__header' ).removeClass( 'hide' );
+                                    break;
+                                case 'bottom':
+                                    $( '.site__header' ).addClass( 'hide' );
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+                    }
+                });
             },
             _move = function( xPercent, yPercent ){
             var phoneStep = 3,
